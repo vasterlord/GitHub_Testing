b1f9cadd4a0bfcc9e1481d112887e604e5df8ab9
vasterlord <vasterlord619@gmail.com>
2017-03-26 20:56:47 +0300

    Set my name

--- GitHub_Testing/GitHub_Testing/Program.cs
+++ GitHub_Testing/GitHub_Testing/Program.cs
@@ -11,6 +11,7 @@ namespace GitHub_Testing
         static void Main(string[] args)
         {
             Person newPersonMy = new Person();
+            newPersonMy.FirsName = "Yulian";
         }
     }
 }
